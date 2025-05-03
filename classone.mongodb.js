ebdb06a71b0e366757bb6ae6eb76aacdcdadfcae
Nayan <dreamerboynayan@gmail.com>
2025-05-03 10:51:12 +0600

    Projection Concept

--- classone.mongodb.js
+++ classone.mongodb.js
@@ -1,11 +1,17 @@
-use("Mern09")
-// Data InsertMany
-db.brand.insertMany([
-    {"name": "Oppo", "color": "Blue"},
-    {"name": "Realme", "color": "Red"},
-    {"name": "Vivo", "color": "White"},
-    {"name": "Redmi", "color": "Orange"},
-])
-//Data Find
-db.brand.find()
+// use("Mern09")
+// // Data InsertMany
+// db.brand.insertMany([
+//     {"name": "Oppo", "color": "Blue"},
+//     {"name": "Realme", "color": "Red"},
+//     {"name": "Vivo", "color": "White"},
+//     {"name": "Redmi", "color": "Orange"},
+// ])
+// //Data Find
+// db.brand.find()
+
+// Projection concept
+
+use("sample_mflix")
+
+db.movies.find({}, {title:1, year:1, _id:0})
 
